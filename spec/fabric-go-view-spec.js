a5a446288490f7f3c379740c6471e24a0957bea4
xuchengli <lixucheng@aliyun.com>
2019-07-24 15:40:55 +0800

    init code

---
+++ spec/fabric-go-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FabricGoView from '../lib/fabric-go-view';
+
+describe('FabricGoView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
